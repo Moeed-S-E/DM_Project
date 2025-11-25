d11099fa0fa0c65cf38f1b5c88404773e89d79e1
Moeed-S-E <muhammadmoeed3737outlook.com>
2025-11-25 17:09:45 +0500

    redeploy3.2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ const geistMono = Geist_Mono({
 });
 
 const poppins = Poppins({
-  weight: ["700", "600"],
+  weight: ["400", "600", "700"],
   subsets: ["latin"],
   variable: "--font-poppins",
   display: "swap",
